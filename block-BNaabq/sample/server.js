86f471f369a1dd8e119e9626e8f3d5ef1966ebc8
Prabhat <prabhatdbg21@gmail.com>
2023-03-26 15:18:23 +0530

    all good

---
+++ block-BNaabq/sample/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+
+var app = express();
+
+
+app.use(logger('dev'))
+
+app.use(express.static(__dirname + '/public'))   
+
+app.use((req, res, next) => {
+    res.cookie("username", "suraj");
+    next()
+})
+
+
+app.get('/about', (req, res) => {
+    res.send('welcome')
+})
+
+
+app.listen(3000, () => {
+    console.log('server is listening on port 3k')
+})
