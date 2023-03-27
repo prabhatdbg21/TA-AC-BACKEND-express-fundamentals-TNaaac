4877b5eede9d09478023c281961bad001467db04
Prabhat <prabhatdbg21@gmail.com>
2023-03-27 10:15:36 +0530

    all good

--- block-BNaabq/sample/server.js
+++ block-BNaabq/sample/server.js
@@ -11,15 +11,14 @@ app.use(express.static(__dirname + '/public'))
 
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-    console.log(req.cookies);
-})
-
 app.use("/about", (req, res, next) => {
     res.cookie("username", "suraj");
     res.end('about page');
 })
 
+app.use((req, res, next) => {
+    console.log(req.cookies);
+})
 
 app.get('/', (req, res) => {
     res.send('welcome')
